66580d9d220b9d740f8141bbc3a84d22bf18837c
Irina Mateush <ismateush@yandex-team.ru>
2020-02-08 20:06:12 +0300

    Поправила замечания

--- js/main.js
+++ js/main.js
@@ -15,49 +15,65 @@ var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getAvatar = function () {
+var getAvatar = function (max, number) {
   var avatars = [];
-  for (var i = 1; i <= count; i++) {
-    avatars[i] = 'img/avatars/user0' + i + '.png';
+  for (var i = 1; i <= max; i++) {
+    avatars.push('img/avatars/user0' + i + '.png');
   }
-  return avatars;
+  return avatars[number];
 };
 
 var getRandomArray = function (arr) {
   var features = [];
-  var count2 = getRandom(1, arr.length - 1);
+  var max = getRandom(1, arr.length - 1);
   arr.sort();
-  for (var i = 0; i < count2; i++) {
+  for (var i = 0; i < max; i++) {
     features.push(arr[i]);
   }
   return features;
 };
 
-for (var i = 0; i < count; i++) {
-  var point = {
-    'author': {
-      'avatar': getAvatar()[i]
-    },
-    'location': {
-      'x': getRandom(0, 1200),
-      'y': getRandom(130, 630)
-    },
-    'offer': {
-      'title': 'Великолепное предложение!',
-      'address': location.x,
-      'price': 100,
-      'type': TYPE[getRandom(0, TYPE.length - 1)],
-      'rooms': 2,
-      'guests': 5,
-      'checkin': CHECKIN[getRandom(0, CHECKIN.length - 1)],
-      'checkout': CHECKOUT[getRandom(0, CHECKOUT.length - 1)],
-      'features': getRandomArray(FEATURES),
-      'description': 'Объект охраняется милыми щенками',
-      'photos': getRandomArray(PHOTOS)
-    }
-  };
-  pins[i] = point;
-}
+var getRandomPins = function (max) {
+  for (var i = 0; i < max; i++) {
+    var x = getRandom(0, 1200);
+    var y = getRandom(130, 630);
+
+    pins.push({
+      author: {
+        avatar: getAvatar(max, i)
+      },
+      location: {
+        x: x,
+        y: y
+      },
+      offer: {
+        title: 'Великолепное предложение!',
+        address: x + ', ' + y,
+        price: getRandom(1500, 30000),
+        type: TYPE[getRandom(0, TYPE.length - 1)],
+        rooms: getRandom(1, 3),
+        guests: getRandom(1, 6),
+        checkin: CHECKIN[getRandom(0, CHECKIN.length - 1)],
+        checkout: CHECKOUT[getRandom(0, CHECKOUT.length - 1)],
+        features: getRandomArray(FEATURES),
+        description: 'Объект охраняется милыми щенками',
+        photos: getRandomArray(PHOTOS)
+      }
+    });
+  }
+  return pins;
+};
+
+var getPin = function (max) {
+  for (var i = 0; i < max; i++) {
+    var pinElement = similarPinTemplate.cloneNode(true);
+    pinElement.setAttribute('style', 'left:' + pins[i].location.x + 'px; top:' + pins[i].location.y + 'px;');
+    pinElement.querySelector('img').setAttribute('src', pins[i].author.avatar);
+    pinElement.querySelector('img').setAttribute('alt', pins[i].offer.title);
+    fragment.appendChild(pinElement);
+  }
+  return fragment;
+};
 
 var pinListElement = document.querySelector('.map__pins');
 var similarPinTemplate = document.querySelector('#pin')
@@ -66,14 +82,8 @@ var similarPinTemplate = document.querySelector('#pin')
 
 var fragment = document.createDocumentFragment();
 
-for (var j = 0; j < count; j++) {
-  var pinElement = similarPinTemplate.cloneNode(true);
-  pinElement.setAttribute('style', 'left:' + pins[i].location.x + 'px; top:' + pins[j].location.y + 'px;'); // как считать? в отдельной ф-ции?
-  pinElement.querySelector('img').setAttribute('src', pins[j].author.avatar);
-  pinElement.querySelector('img').setAttribute('alt', pins[j].offer.title);
-  fragment.appendChild(pinElement);
-}
-
+getRandomPins(count);
+getPin(count);
 pinListElement.appendChild(fragment);
 
 
