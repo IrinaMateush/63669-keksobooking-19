84a556eb8e58017c5388db9cfee794d45537b076
Irina Mateush <ismateush@yandex-team.ru>
2020-02-09 16:49:03 +0300

    Поправила замечания

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var pins = [];
-var count = 8;
+var COUNT = 8;
+var MIN_COST = 1500;
+var MAX_COST = 30000;
+var MIN_GUEST = 1;
+var MAX_GUEST = 6;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 3;
+var MIN_Y = 130;
+var MAX_Y = 630;
+var MIN_X = 0;
+var MAX_X = 1200;
 
 var mapElement = document.querySelector('.map');
 mapElement.classList.remove('map--faded');
@@ -15,12 +25,9 @@ var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getAvatar = function (max, number) {
-  var avatars = [];
-  for (var i = 1; i <= max; i++) {
-    avatars.push('img/avatars/user0' + i + '.png');
-  }
-  return avatars[number];
+var getAvatar = function (index) {
+  var photoIndex = index + 1;
+  return 'img/avatars/user0' + photoIndex + '.png';
 };
 
 var getRandomArray = function (arr) {
@@ -35,12 +42,12 @@ var getRandomArray = function (arr) {
 
 var getRandomPins = function (max) {
   for (var i = 0; i < max; i++) {
-    var x = getRandom(0, 1200);
-    var y = getRandom(130, 630);
+    var x = getRandom(MIN_X, MAX_X);
+    var y = getRandom(MIN_Y, MAX_Y);
 
     pins.push({
       author: {
-        avatar: getAvatar(max, i)
+        avatar: getAvatar(i)
       },
       location: {
         x: x,
@@ -49,10 +56,10 @@ var getRandomPins = function (max) {
       offer: {
         title: 'Великолепное предложение!',
         address: x + ', ' + y,
-        price: getRandom(1500, 30000),
+        price: getRandom(MIN_COST, MAX_COST),
         type: TYPE[getRandom(0, TYPE.length - 1)],
-        rooms: getRandom(1, 3),
-        guests: getRandom(1, 6),
+        rooms: getRandom(MIN_ROOMS, MAX_ROOMS),
+        guests: getRandom(MIN_GUEST, MAX_GUEST),
         checkin: CHECKIN[getRandom(0, CHECKIN.length - 1)],
         checkout: CHECKOUT[getRandom(0, CHECKOUT.length - 1)],
         features: getRandomArray(FEATURES),
@@ -64,10 +71,12 @@ var getRandomPins = function (max) {
   return pins;
 };
 
-var getPin = function (max) {
+var getPinElement = function (max) {
+  var pinHalfWidth = 23;
+  var pinHeight = 64;
   for (var i = 0; i < max; i++) {
     var pinElement = similarPinTemplate.cloneNode(true);
-    pinElement.setAttribute('style', 'left:' + pins[i].location.x + 'px; top:' + pins[i].location.y + 'px;');
+    pinElement.setAttribute('style', 'left:' + (pins[i].location.x - pinHalfWidth) + 'px; top:' + (pins[i].location.y - pinHeight) + 'px;');
     pinElement.querySelector('img').setAttribute('src', pins[i].author.avatar);
     pinElement.querySelector('img').setAttribute('alt', pins[i].offer.title);
     fragment.appendChild(pinElement);
@@ -82,8 +91,8 @@ var similarPinTemplate = document.querySelector('#pin')
 
 var fragment = document.createDocumentFragment();
 
-getRandomPins(count);
-getPin(count);
+getRandomPins(COUNT);
+getPinElement(COUNT);
 pinListElement.appendChild(fragment);
 
 
